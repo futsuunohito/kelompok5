e4c983e9f693814ad84955130a1c5c318229943f
iyangankwibowo <iyang_wibowo@apps.ipb.ac.id>
2018-05-15 16:36:00 +0700

    Iyang

--- resources/views/employer/employer_login.blade.php
+++ resources/views/employer/employer_login.blade.php
@@ -141,7 +141,7 @@
                 <div class="row">
                   <div class="col-lg-6"><a href="{{ route('employer.register')}}" class="btn btn-primary form-control" style="text-transform: uppercase;border-radius:0;">Sign up as Seeker</a></div>
                   <div class="col-lg-6"><a href="{{ route('seeker.register')}}" class="btn btn-default form-control" style="text-transform: uppercase;border-radius:0;">Sign up as Helper</a></div>
-                  <br><br><br>
+                  <br><br><br>  
                 </div>
             </div>
         </div>   
