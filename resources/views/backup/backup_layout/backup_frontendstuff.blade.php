2fe1d731df05641b30bc611fc680d5370072d8b9
iyangankwibowo <iyang_wibowo@apps.ipb.ac.id>
2018-05-15 22:13:00 +0700

    Iyang

---
+++ resources/views/backup/backup_layout/backup_frontendstuff.blade.php
@@ -0,0 +1,381 @@
+<!-- HTML FOR REGISTER -->
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+
+<div class="container">
+    
+
+    <div class="onl_login">
+        <h3 class="onl_authTitle" data-toggle="tooltip" data-placement="bottom" title="Recommended: Lumen Theme">Login or <a href="#">Sign up</a></h3>
+		<div class="row onl_row-sm-offset-3 onl_socialButtons">
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-facebook" data-toggle="tooltip" data-placement="top" title="Facebook">
+              <i class="fa fa-facebook fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-twitter" data-toggle="tooltip" data-placement="top" title="Twitter">
+              <i class="fa fa-twitter fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>  
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-google-plus" data-toggle="tooltip" data-placement="top" title="Google Plus">
+              <i class="fa fa-google-plus fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>  
+    </div><br>
+
+    <div class="row onl_row-sm-offset-3 onl_socialButtons">
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-github" data-toggle="tooltip" data-placement="top" title="GitHub">
+              <i class="fa fa-github fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-soundcloud" data-toggle="tooltip" data-placement="top" title="SoundCloud">
+              <i class="fa fa-soundcloud fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>  
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-steam" data-toggle="tooltip" data-placement="top" title="Steam">
+              <i class="fa fa-steam fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div> 
+    </div><br>
+    <div class="row onl_row-sm-offset-3 onl_socialButtons">
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-pinterest" data-toggle="tooltip" data-placement="top" title="Pinterest">
+              <i class="fa fa-pinterest fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-vimeo" data-toggle="tooltip" data-placement="top" title="Vimeo">
+              <i class="fa fa-vimeo-square fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>  
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-lastfm" data-toggle="tooltip" data-placement="top" title="Lastfm">
+              <i class="fa fa-lastfm fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div> 
+    </div><br>
+    <div class="row onl_row-sm-offset-3 onl_socialButtons">
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-yahoo" data-toggle="tooltip" data-placement="top" title="Yahoo">
+              <i class="fa fa-yahoo fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-vk" data-toggle="tooltip" data-placement="top" title="VKontakte">
+              <i class="fa fa-vk fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>  
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-spotify" data-toggle="tooltip" data-placement="top" title="Spotify">
+              <i class="fa fa-spotify fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div> 
+    </div><br>
+    <div class="row onl_row-sm-offset-3 onl_socialButtons">
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-linkedin" data-toggle="tooltip" data-placement="top" title="LinkedIn">
+              <i class="fa fa-linkedin fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-stumbleupon" data-toggle="tooltip" data-placement="top" title="Stumble Upon">
+              <i class="fa fa-stumbleupon fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>  
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block onl_btn-tumblr" data-toggle="tooltip" data-placement="top" title="Tumblr">
+              <i class="fa fa-tumblr fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div> 
+    </div>
+
+		<div class="row onl_row-sm-offset-3 onl_loginOr">
+			<div class="col-xs-12 col-sm-6">
+				<hr class="onl_hrOr">
+				<span class="onl_spanOr">or</span>
+			</div>
+		</div>
+
+		<div class="row onl_row-sm-offset-3">
+			<div class="col-xs-12 col-sm-6">	
+			    <form class="onl_loginForm" action="" autocomplete="off" method="POST">
+					<div class="input-group">
+						<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+						<input type="text" class="form-control" name="username" placeholder="Username">
+					</div>
+					<span class="help-block"></span>
+										
+					<div class="input-group">
+						<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+						<input  type="password" class="form-control" name="password" placeholder="Password">
+					</div>
+                    <span class="help-block">Password Error!</span>
+
+					<button class="btn btn-lg btn-primary btn-block" type="submit"><i class="fa fa-sign-in"></i> Login</button>
+				</form>
+			</div>
+    	</div>
+		<div class="row onl_row-sm-offset-3">
+			<div class="col-xs-12 col-sm-3">
+				<label class="checkbox">
+					<input type="checkbox" value="remember-me">Remember Me
+				</label>
+			</div>
+			<div class="col-xs-12 col-sm-3">
+				<p class="onl_forgotPwd">
+					<a href="#">Forgot password?</a>
+				</p>
+			</div>
+		</div>	    	
+	</div>
+
+
+
+        </div>
+        <script>
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+</script>
+
+
+
+
+
+
+
+
+<!-- HTML FOR LOGIN -->
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>google-search</title>
+
+<link rel="stylesheet prefetch" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet prefetch" href="https://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+
+
+</head>
+<body>
+<div class="fluid-container">
+  <header class="search-header">
+  
+  <div class="col-md-2"><div class="logo-top">logo</div></div> 
+  
+  <div class="col-md-5"><div id="custom-search-input">
+        <div class="row">
+          <div class="col-md-12">
+            <input class="search-query form-control" placeholder="Search product" type="text">
+            <span class="input-group-btn">
+            <button class="btn btn-danger" type="button"> <span class=" glyphicon glyphicon-search"></span> </button>
+            </span> </div>
+            </div>
+  </div></div>
+  
+  <div class="col-md-4 pull-right">
+       
+  <div class="login-register">
+  <ul>
+  <li> <a href="#"> <i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>  </li>
+  <li> <a href="#"> <i class="fa fa-user" aria-hidden="true"></i> Register</a> </li>
+  <li> 
+  
+  <div class="dropdown-widget">
+  <ul class="nav navbar-nav">
+      <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <svg aria-hidden="true" data-prefix="fas" data-icon="th" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-th fa-w-16" style="font-size: 48px;"><path fill="currentColor" d="M149.333 56v80c0 13.255-10.745 24-24 24H24c-13.255 0-24-10.745-24-24V56c0-13.255 10.745-24 24-24h101.333c13.255 0 24 10.745 24 24zm181.334 240v-80c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24h101.333c13.256 0 24.001-10.745 24.001-24zm32-240v80c0 13.255 10.745 24 24 24H488c13.255 0 24-10.745 24-24V56c0-13.255-10.745-24-24-24H386.667c-13.255 0-24 10.745-24 24zm-32 80V56c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24h101.333c13.256 0 24.001-10.745 24.001-24zm-205.334 56H24c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24zM0 376v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H24c-13.255 0-24 10.745-24 24zm386.667-56H488c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H386.667c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24zm0 160H488c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H386.667c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24zM181.333 376v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24z" class=""></path></svg> </a>
+          <ul class="dropdown-menu">
+            <li><a href="#">  Account Settings  </a></li>
+            <li><a href="#">  User stats </a></li>
+            <li><a href="#">  Messages </a></li>
+            <li><a href="#">  Favourites Snippets</a></li>
+            <li><a href="#">  Sign Out</a></li>
+            <li><a href="#">  Sign Out</a></li>
+          </ul>
+        </li>
+      </ul>
+    
+</div>
+
+</li>
+  </ul>
+  </div>
+  
+
+  
+  </div>
+  </header>
+  
+
+</div>
+
+<!--footer -->
+
+
+
+<!--footer /-->
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script> 
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js'></script> 
+<script  src="js/index.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+<!-- HTML FOR SEARCH -->
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<nav>
+    <ul>
+      <li><a href="#" class="active">Explore</a></li>
+    </ul>
+  </div>				
+</nav>
+<div class="categories animated animated-fast fadeInUp">
+  <div class="title">Categories</div>
+  <a href="#">Activities</a>
+  <a href="#">Artists</a>
+  <a href="#">Childcare</a>
+  <a href="#">General</a>
+  <a href="#">Groups</a>
+  <a href="#">Pets</a>
+  <a href="#">Events</a>
+  <a href="#">Lost+Found</a>
+  <a href="#">Musicians</a>
+  <a href="#">Local News</a>
+  <a href="#">Politics</a>
+  <a href="#">Rideshare</a>
+  <a href="#">Volunteers</a>
+  <a href="#">Classes</a>
+</div>
+<script>
+    $('nav a').click(function(){
+  if ($(this).hasClass('active')){
+    $('.categories')
+      .removeClass('fadeInUp')
+      .addClass('fadeOutDown');
+    $(this).removeClass('active');
+  } else {
+    $('.categories')
+      .removeClass('fadeOutDown')
+      .addClass('fadeInUp');
+    $(this).addClass('active');
+  }
+});
+</script>
+
+<!-- HTML FOR NAVBAR -->
+
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<div id="flipkart-navbar">
+    <div class="container">
+        <div class="row row1">
+            <ul class="largenav pull-right">
+                <li class="upper-links"><a class="links" href="http://clashhacks.in/">Link 1</a></li>
+                <li class="upper-links"><a class="links" href="https://campusbox.org/">Link 2</a></li>
+                <li class="upper-links"><a class="links" href="http://clashhacks.in/">Link 3</a></li>
+                <li class="upper-links"><a class="links" href="http://clashhacks.in/">Link 4</a></li>
+                <li class="upper-links"><a class="links" href="http://clashhacks.in/">Link 5</a></li>
+                <li class="upper-links"><a class="links" href="http://clashhacks.in/">Link 6</a></li>
+                <li class="upper-links">
+                    <a class="links" href="http://clashhacks.in/">
+                        <svg class="" width="16px" height="12px" style="overflow: visible;">
+                            <path d="M8.037 17.546c1.487 0 2.417-.93 2.417-2.417H5.62c0 1.486.93 2.415 2.417 2.415m5.315-6.463v-2.97h-.005c-.044-3.266-1.67-5.46-4.337-5.98v-.81C9.01.622 8.436.05 7.735.05 7.033.05 6.46.624 6.46 1.325v.808c-2.667.52-4.294 2.716-4.338 5.98h-.005v2.972l-1.843 1.42v1.376h14.92v-1.375l-1.842-1.42z" fill="#fff"></path>
+                        </svg>
+                    </a>
+                </li>
+                <li class="upper-links dropdown"><a class="links" href="http://clashhacks.in/">Dropdown</a>
+                    <ul class="dropdown-menu">
+                        <li class="profile-li"><a class="profile-links" href="http://yazilife.com/">Link</a></li>
+                        <li class="profile-li"><a class="profile-links" href="http://hacksociety.tech/">Link</a></li>
+                        <li class="profile-li"><a class="profile-links" href="http://clashhacks.in/">Link</a></li>
+                        <li class="profile-li"><a class="profile-links" href="http://clashhacks.in/">Link</a></li>
+                        <li class="profile-li"><a class="profile-links" href="http://clashhacks.in/">Link</a></li>
+                        <li class="profile-li"><a class="profile-links" href="http://clashhacks.in/">Link</a></li>
+                        <li class="profile-li"><a class="profile-links" href="http://clashhacks.in/">Link</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="row row2">
+            <div class="col-sm-2">
+                <h2 style="margin:0px;"><span class="smallnav menu" onclick="openNav()">☰ Brand</span></h2>
+                <h1 style="margin:0px;"><span class="largenav">Brand</span></h1>
+            </div>
+            <div class="flipkart-navbar-search smallsearch col-sm-8 col-xs-11">
+                <div class="row">
+                    <input class="flipkart-navbar-input col-xs-11" type="" placeholder="Search for Products, Brands and more" name="">
+                    <button class="flipkart-navbar-button col-xs-1">
+                        <svg width="15px" height="15px">
+                            <path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div class="cart largenav col-sm-2">
+                <a class="cart-button">
+                    <svg class="cart-svg " width="16 " height="16 " viewBox="0 0 16 16 ">
+                        <path d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86 " fill="#fff "></path>
+                    </svg> Link
+                    <span class="item-number ">0</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="mySidenav" class="sidenav">
+    <div class="container" style="background-color: #2874f0; padding-top: 10px;">
+        <span class="sidenav-heading">Home</span>
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+    </div>
+    <a href="http://clashhacks.in/">Link</a>
+    <a href="http://clashhacks.in/">Link</a>
+    <a href="http://clashhacks.in/">Link</a>
+    <a href="http://clashhacks.in/">Link</a>
+</div>
