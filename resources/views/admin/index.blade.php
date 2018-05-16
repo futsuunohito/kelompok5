5c3e1c6ff17342dbc63d5e896720547f189ade49
allifahr <allifahrahmatillah98@gmail.com>
2018-05-16 10:32:53 +0700

    allifah

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -32,7 +32,7 @@
 
 <div class="container">
 	<div class="row">
-      <div class="col-md-4 col-md-offset-4">
+      <div class="col-md-9 col-md-offset-4">
         <div class="material-button-anim">
           <ul class="list-inline" id="options">
             <li class="option">
