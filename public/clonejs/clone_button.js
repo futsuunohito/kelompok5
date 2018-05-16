502ead974b910e34dc23a965ce71d478675dbc90
iyangankwibowo <iyang_wibowo@apps.ipb.ac.id>
2018-05-16 10:08:10 +0700

    Iyang

--- public/clonejs/clone_button.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.material-button-toggle').on("click", function () {
-        $(this).toggleClass('open');
-        $('.option').toggleClass('scale-on');
-    });
-});
